0cc0ac42daa0bef507f3b92bf26fdbe03f21584d
Naghub-coder <68456021+Naghub-coder@users.noreply.github.com>
2022-01-27 21:16:59 +0530

    seconf commit

--- CRUD_APP/server/database/connection.js
+++ CRUD_APP/server/database/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/cats_app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+
 
 const connectDB = async () => {
     try{
